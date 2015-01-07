38fbaddf3298a07e0fa07f8e92049488cac216e4
LucasMW <great-lucas@hotmail.com>
2015-01-07 13:42:44 -0200

    Great Advance

--- MW_File_Iterator.h
+++ MW_File_Iterator.h
@@ -13,7 +13,7 @@
 #include <stdlib.h>
 
 typedef struct MW_FI_tagFI * MW_FI_tppFI ;
-typedef int MW_Int;
+typedef long long int MW_Int;
 typedef enum {
     
     MW_FI_CondRetOK ,
@@ -22,13 +22,16 @@ typedef enum {
     /* There`s no memory left */
     MW_FI_CondRetFileNotFound ,
     /* File couldn`t be opened correctly */
+    MW_FI_CondRetEOF
+    /* End Of File */
     
     
 } MW_FI_tpCondRet ;
 
 MW_FI_tpCondRet MW_FI_CreateIterator( MW_FI_tppFI* refFI, char* inputPath, char* outputPath, MW_Int sizeOfData);
-MW_FI_tpCondRet MW_FI_GetData(MW_FI_tppFI iterator, void** refVector  );
-
-
+MW_FI_tpCondRet MW_FI_GetData(MW_FI_tppFI iterator, void** refVector , MW_Int * refSizeOfVector );
+MW_FI_tpCondRet MW_FI_AdvanceWriting(MW_FI_tppFI iterator);
+MW_FI_tpCondRet MW_FI_AdvanceReading(MW_FI_tppFI iterator);
+MW_FI_tpCondRet MW_FI_DestroyIterator(MW_FI_tppFI iterator);
 
 #endif /* defined(__MW_File_Iterator__MW_File_Iterator__) */
---
+++ main.c
@@ -0,0 +1,9 @@
+//
+//  main.c
+//  MW_File_Iterator
+//
+//  Created by Lucas Menezes on 06/01/15.
+//  Copyright (c) 2015 Lucas Menezes. All rights reserved.
+//
+
+#include <stdio.h>
